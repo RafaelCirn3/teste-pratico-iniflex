450bffa274fbf032d6081f0099a64db772735d7a
Rafael Cirne <125037856+RafaelCirn3@users.noreply.github.com>
2026-09-21 16:14:02 -0300

    feat(model): adiciona entidade Funcionario

---
+++ src/main/java/br/com/iniflex/model/Funcionario.java
@@ -0,0 +1,34 @@
+package br.com.iniflex.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Funcionario extends Pessoa {
+
+    private BigDecimal salario;
+    private final String funcao;
+
+    public Funcionario(
+            String nome,
+            LocalDate dataNascimento,
+            BigDecimal salario,
+            String funcao
+    ) {
+        super(nome, dataNascimento);
+        this.salario = Objects.requireNonNull(salario, "O salário não pode ser nulo");
+        this.funcao = Objects.requireNonNull(funcao, "A função não pode ser nula");
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = Objects.requireNonNull(salario, "O salário não pode ser nulo");
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+}
